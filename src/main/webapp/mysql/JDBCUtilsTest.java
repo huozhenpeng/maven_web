8411841e911cef3c7c1126e60d86a2e9f4e10e3f
霍振鹏 <huozhenpengdyx@gmail.com>
2020-11-23 20:47:13 +0800

    jdbc事务

--- src/main/webapp/mysql/JDBCUtilsTest.java
+++ src/main/webapp/mysql/JDBCUtilsTest.java
@@ -19,8 +19,34 @@ public class JDBCUtilsTest {
             int count = statement.executeUpdate(sql);
             System.out.println(count);
             JDBCUtils.close(statement, connection);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException th) {
+            th.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testTransaction() {
+        Statement statement = null;
+        Connection connection = null;
+        try {
+            connection = JDBCUtils.getConnection();
+            //开启事务
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
+            String sql = "update account set balance = 2000 where id = 1";
+            int count = statement.executeUpdate(sql);
+            System.out.println(count);
+            connection.commit();
+
+        } catch (SQLException th) {
+            th.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        } finally {
+            JDBCUtils.close(statement, connection);
         }
     }
 }
